aa0de8ecd87adc671b113d6e467b5966175849bc
Hooriya M. Fareed <parih3080@gmail.com>
2025-12-15 00:35:53 +0500

    push_changes

--- backend/src/main.py
+++ backend/src/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
         os.getenv("FRONTEND_URL", "http://localhost:3000")
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
